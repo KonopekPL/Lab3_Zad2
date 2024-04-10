35916fd3c80ea38d3d44fac3542b76654397155d
KonopekPL <162459532+KonopekPL@users.noreply.github.com>
2024-04-10 13:34:57 +0000

    v3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,14 +6,27 @@ public class Main {
         System.out.print("Podaj długość boku: ");
         int bok = scanner.nextInt();
 
-
+        // Rysowanie trójkąta normalnego
+        System.out.println("Trójkąt normalny:");
         for (int i = 0; i < bok; i++) {
             for (int j = 0; j <= i; j++) {
-
                 if (i == bok - 1 || j == 0 || j == i) {
                     System.out.print("* ");
                 } else {
                     System.out.print("  "); 
+                }
+            }
+            System.out.println();
+        }
+
+        // Rysowanie trójkąta odwróconego w poziomie
+        System.out.println("\nTrójkąt odwrócony w poziomie:");
+        for (int i = 0; i < bok; i++) {
+            for (int j = 0; j < bok; j++) {
+                if (j >= i) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
                 }
             }
             System.out.println();
